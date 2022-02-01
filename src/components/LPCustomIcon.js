94450c18450dc5d9f59ce5532719374172827d03
Sunil Kumar <sunil.khuwal@gmail.com>
2022-02-01 21:25:49 +0530

    removed unwanted style

--- src/components/LPCustomIcon.js
+++ src/components/LPCustomIcon.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {SvgXml} from 'react-native-svg';
 import SVG_ICONS from '../statics/SvgIcons';
 import COLORS from '../statics/colors';
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
 
 /**
  * This component is used to display the svg based items those can be configured based on
@@ -42,24 +42,4 @@ LPCustomIcon.defaultProps = {
   color: COLORS.CERISE_RED,
 };
 
-const main = StyleSheet.create({
-  unreadCount: {
-    position: 'absolute',
-    top: -12,
-    right: -5,
-    height: 20,
-    width: 20,
-    borderRadius: 10,
-    backgroundColor: COLORS.CERISE_RED_1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  unreadCountText: {
-    fontSize: 11,
-    color: COLORS.WHITE,
-  },
-});
-const styles = {
-  main,
-};
 export default LPCustomIcon;
